458a018c950650c172bdb1ce01e614756da156e8
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-11-01 22:06:56 +0800

    Push UI

--- src/components/Server.jsx
+++ src/components/Server.jsx
@@ -1,9 +1,11 @@
 const Server = () => {
-  return (
-    <div>
-      <h1>Server</h1>
-    </div>
-  );
+    const buttonClass = 'border-[1px] rounded-lg border-black py-2 px-4 m-1';
+    return (
+        <div className="w-full min-h-screen flex flex-col justify-center items-center">
+            <h1>You are the Lecturer!</h1>
+            <button className={buttonClass}>Start audio</button>
+        </div>
+    );
 };
 
 export default Server;
